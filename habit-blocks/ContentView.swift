8874060f1cd2cfd3d3859114656794eb9bb93443
Rose Soriano <rosesoriano.dtx@gmail.com>
2025-01-08 23:33:39 -0800

    Create a new iOS template Swift project

---
+++ habit-blocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habit-blocks
+//
+//  Created by Rose Soriano on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habit-blocks/habit_blocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_blocksApp.swift
+//  habit-blocks
+//
+//  Created by Rose Soriano on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct habit_blocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
